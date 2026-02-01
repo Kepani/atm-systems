c23e23ddc3bff5cbe768bf59ed510ddb7b171334
OhhLenzo <foster.brendan01@gmail.com>
2026-02-01 10:19:37 -1000

    added mock_back sim

---
+++ atm/services/mock_bank.py
@@ -0,0 +1,14 @@
+class MockBankService:
+    def __init__(self):
+        self.accounts = {
+            "123": {
+                "pin": "1111",
+                "balance": 1000
+            }
+        }
+
+    def validate_pin(self, card_id, pin):
+        return (
+            card_id in self.accounts
+            and self.accounts[card_id]["pin"] == pin
+        )
